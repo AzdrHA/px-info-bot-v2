7739250bc1d9e333d10d6608ee9cbb929747884f
AzdrHA <azdracito@gmail.com>
2023-03-29 16:17:14 +0200

    Fix exception

--- src/collector/MessageCollector.ts
+++ src/collector/MessageCollector.ts
@@ -1,8 +1,10 @@
 import {
   type ButtonInteraction,
   Message,
-  type MessageCollector as BaseMessageCollector
+  type MessageCollector as BaseMessageCollector, type TextChannel
 } from 'discord.js'
+import { type Callback } from '@abstract/AbstractAction'
+import ExceptionService from '@service/ExceptionService'
 
 /**
  * @public
@@ -12,7 +14,7 @@ export class MessageCollector {
   private readonly clientMessage: Message
   private readonly executorMessage: Message | ButtonInteraction
   private collector: BaseMessageCollector | undefined
-  private readonly callback: ((content: string) => void) | undefined
+  private readonly callback: Callback | undefined
 
   /**
    * @public
@@ -23,7 +25,7 @@ export class MessageCollector {
   public constructor (
     clientMessage: Message,
     executorMessage: Message | ButtonInteraction,
-    callback?: (content: string) => void
+    callback?: Callback
   ) {
     this.clientMessage = clientMessage
     this.executorMessage = executorMessage
@@ -59,7 +61,11 @@ export class MessageCollector {
     // Delete the message from the user
     if (message.deletable) await message.delete().catch(() => null)
     // Call the callback if it exists
-    if (this.callback != null) this.callback(message.content)
+    try {
+      if (this.callback != null) await this.callback(message.content)
+    } catch (e) {
+      return new ExceptionService(e as Error, this.executorMessage.channel as TextChannel)
+    }
   }
 
   public getCollector = (): BaseMessageCollector | undefined => {
